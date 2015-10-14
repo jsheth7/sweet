861a10d5ee898221ddfd27e5090ba684b53929a7
Jay Sheth <js_github@fastmail.fm>
2015-10-13 23:00:25 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+require 'vendor/autoload.php';
+
+// Create and configure Slim app
+$app = new \Slim\App;
+
+// Define app routes
+$app->get('/hello/{name}', function ($request, $response, $args) {
+    return $response->write("Hello " . $args['name']);
+});
+
+// Run app
+$app->run();
