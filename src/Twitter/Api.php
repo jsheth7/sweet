60367498c4957464dfa4520cf53ab9a4c514e045
Jay Sheth <js_github@fastmail.fm>
2015-10-15 20:03:26 -0400

    refactor code for reuse & readability

--- src/Twitter/Api.php
+++ src/Twitter/Api.php
@@ -8,11 +8,38 @@ class Api
 
     protected $oAuthTokenUrl;
 
+    protected $token;
+    protected $httpClient;
+
     public function __construct(){
+        $this->init();
+    }
 
-        $this->oAuthTokenUrlBase = 'https://api.twitter.com';
-        $this->oAuthTokenUrlPath = '/oauth2/token';
+    protected function init(){
+        $this->apiUrlBase = 'https://api.twitter.com';
+        $this->oAuthTokenPath = '/oauth2/token';
+        $this->timelinePath = '/1.1/statuses/user_timeline.json';
 
+        $this->httpClient = new Client([
+            // Base URI is used with relative requests
+            'base_uri' => $this->apiUrlBase,
+
+        ]);
+        $this->token = $this->getToken();
+    }
+
+    /**
+     * Make an API request to Twitter's end-point
+     * @param $httpMethod - GET / POST
+     * @param $path /path/to/method
+     * @param $requestParams array('key' => 'value')
+     * @return string
+     */
+    protected function makeHttpRequest($httpMethod, $path, $requestParams ){
+        $response = $this->httpClient->request($httpMethod, $path, $requestParams);
+        $responseBodyJson = $response->getBody();
+        $responseBody = json_decode($responseBodyJson, true);
+        return $responseBody;
     }
 
     /**
@@ -27,27 +54,26 @@ class Api
             throw new \Exception('Please make sure consumer key and secret are set');
         }
 
-        $client = new Client([
-    	// Base URI is used with relative requests
-    	'base_uri' => $this->oAuthTokenUrlBase,
-   
-		]);
-
         $requestParams = [ 'auth' => [ $consumerKey , $consumerSecret ],
                            'form_params' => [ 'grant_type' => 'client_credentials' ] ];
 
-        $response = $client->request('POST', $this->oAuthTokenUrlPath, $requestParams);
-        $responseBodyJson = $response->getBody();
-        $responseBody = json_decode($responseBodyJson, true);
-        
+        $responseBody = $this->makeHttpRequest('POST', $this->oAuthTokenPath, $requestParams);
         return $responseBody['access_token'];
     }
 
+    /**
+     * Get the latest x tweets for a user
+     * @see https://dev.twitter.com/rest/public/timelines
+     * @see https://dev.twitter.com/rest/reference/get/statuses/user_timeline
+     * @param $username
+     */
+    public function getLatestTweets($username, $numTweets){
+        //$apiUrl = $this->apiUrlBase . $this->timelinePath;
+
+    }
+
     public function search(){
-
-        $token = $this->getToken();
-
-        return array( array('token' => $token, 'title' => 'hello world 2', 'body' => 'This is #helloworld 2!') );
+        return array( array('token' => $this->token, 'title' => 'hello world 2', 'body' => 'This is #helloworld 2!') );
     }
 
 }
